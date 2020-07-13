5f7043168e7aaec9b43f1dac4580667b50dd50d0
Nolen Johnson <johnsonnolen@gmail.com>
2020-07-13 18:35:22 -0400

    potter: Don't declare BOARD_VENDOR
    * Only matters when it differs from the device OEM.
    
    Change-Id: Id9af8263cb42dc7a0e3a9ce8677385e224910302

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,8 +21,6 @@ DEVICE_PATH := device/motorola/potter
 
 TARGET_SPECIFIC_HEADER_PATH := $(DEVICE_PATH)/include
 
-BOARD_VENDOR := motorola-qcom
-
 # AIDs and CAPS
 TARGET_FS_CONFIG_GEN += \
     $(DEVICE_PATH)/config.fs \

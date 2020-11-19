a7e0c6ad6747235cfe169a171979f9716593f27c
nift4 <nift4@protonmail.com>
2020-11-19 16:06:06 +0100

    potter: Remove unneeded sensor config and package
    * Remove unused sensors.rp (our sensor HAL handles the rear proximity sensor)
    * Remove unused qcom sensor config
    
    Change-Id: If3faecba76c1ee009c1daefdc6a0cf69b8f16aeb

--- device.mk
+++ device.mk
@@ -86,11 +86,9 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    motosh \
-    sensors.rp
+    motosh
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
 # Thermal

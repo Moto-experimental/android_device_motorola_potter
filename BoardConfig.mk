374f91c67fdda6a66fcfe3304c0909f4ffce964d
Kevin F. Haggerty <haggertk@lineageos.org>
2021-04-09 08:18:37 -0600

    potter: Don't let builds complete without vendor tree
    * There is zero reason to ever build without blobs. We've even seen
      this situation with official builds from our servers. It's always
      better for a build to fail than it is for it to produce something
      that has no chance at working.
    
    Change-Id: I4968795670c91f691e9ecdc0e4af62e16ba3a93a
    Signed-off-by: SirRGB <sirrgb@proton.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,7 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
--include vendor/motorola/potter/BoardConfigVendor.mk
+include vendor/motorola/potter/BoardConfigVendor.mk
 
 DEVICE_PATH := device/motorola/potter
 
